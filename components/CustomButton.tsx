19403ec9716e83dafe57aa4ff709b9fa805893cb
Akiode Timothy <akiodetimothy2017@gmai.com>
2024-04-11 13:38:41 +0100

    card card and some other stuffs

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -5,8 +5,10 @@ import Image from "next/image";
 const CustomButton = ({
   title,
   containerStyles,
-  handleClick, 
-  btnType
+  handleClick,
+  btnType,
+  textStyles,
+  rightIcon,
 }: CustomButtonProps) => {
   return (
     <button
@@ -15,7 +17,17 @@ const CustomButton = ({
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-      <span className={`flex-1`}>{title}</span>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="Right Icon"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
   );
 };
